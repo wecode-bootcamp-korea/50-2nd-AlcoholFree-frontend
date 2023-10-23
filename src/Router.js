a9d0061b44da602d3691d320b40cee9dc340d909
shin.heehyun <lupinworld22@gmail.com>
2023-10-23 17:36:33 +0900

    Modify: 초기 세팅 리뷰 반영

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 import Detail from './pages/Detail/Detail';
 import Cost from './pages/Cost/Cost';
@@ -10,13 +11,14 @@ import Cart from './pages/Cart/Cart';
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Detail" element={<Detail />} />
-        <Route path="/Cost" element={<Cost />} />
-        <Route path="/Cart" element={<Cart />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/cost" element={<Cost />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://10.58.5.126:8000';
+
+const API = {
+  product: `${BASE_URL}/products`,
+};
+
+export default API;
