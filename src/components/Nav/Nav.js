124c20c1075812fc3b3edfc5a5afb4b256ba75aa
shin.heehyun <lupinworld22@gmail.com>
2023-10-26 18:18:52 +0900

    Nav 컴포넌트 UI 구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import tabLogo from './AlcoholFreeLogo.png';
+import './Nav.scss';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <div className="tabheader">
+        <div className="topLink">고객센터</div>
+        <div className="topLink">마이페이지</div>
+        <div className="topLink">관심상품</div>
+        <div className="topLink">알림</div>
+        <div className="topLink">로그인</div>
+      </div>
+
+      <div className="tab">
+        <div className="tabLogoImage">
+          <img src={tabLogo} alt="로고" />
+        </div>
+
+        <div className="tabBody">
+          <div className="mainLink">HOME</div>
+          <div className="mainLink">STYLE</div>
+          <div className="mainLink">SHOP</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
