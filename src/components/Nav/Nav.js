90372a2e24434af629dbcfe7baa505db54bace91
shin.heehyun <lupinworld22@gmail.com>
2023-11-01 17:27:57 +0900

    Nav Header 창(컴포넌트)  구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,48 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import tabLogo from './AlcoholFreeLogo.png';
 import './Nav.scss';
 
 const Nav = () => {
+  const navigate = useNavigate();
+
+  const goLogin = () => {
+    navigate('/login');
+  };
+
+  const goHome = () => {
+    navigate('/');
+  };
+
+  const goCart = () => {
+    navigate('/cost');
+  };
+
   return (
     <div className="nav">
       <div className="tabheader">
-        <div className="topLink">고객센터</div>
-        <div className="topLink">마이페이지</div>
-        <div className="topLink">관심상품</div>
-        <div className="topLink">알림</div>
-        <div className="topLink">로그인</div>
+        <button className="topLink">고객센터</button>
+        <button className="topLink">마이페이지</button>
+        <button className="topLink">관심상품</button>
+        <button className="topLink">알림</button>
+        <button className="topLink" onClick={goLogin}>
+          로그인
+        </button>
       </div>
 
       <div className="tab">
         <div className="tabLogoImage">
-          <img src={tabLogo} alt="로고" />
+          <img src={tabLogo} alt="로고" onClick={goHome} />
         </div>
 
         <div className="tabBody">
-          <div className="mainLink">HOME</div>
-          <div className="mainLink">STYLE</div>
-          <div className="mainLink">SHOP</div>
+          <button className="mainLink" onClick={goHome}>
+            SHOP
+          </button>
+          <button className="mainLink">TABLE</button>
+          <button className="mainLink" onClick={goCart}>
+            CART & PAY
+          </button>
         </div>
       </div>
     </div>
